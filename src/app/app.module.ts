f72520e323336ac6f9d88ef73ed185d98ad1c605
Aron Harsfalvi <hronnie@gmail.com>
2017-08-28 06:34:06 +0200

    trying to add new module/component - doesn't work yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { LoginRegModule } from './login-reg/login-reg.module';
 
 import { AppComponent } from './app.component';
+//import { LoginComponent } from './login-reg/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent//,
+    //LoginComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpModule
+    BrowserModule, 
+    FormsModule, 
+    HttpModule, 
+    LoginRegModule
   ],
   providers: [],
   bootstrap: [AppComponent]
