40caa0f604917089dba35fd393697ed912205604
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2017-10-11 19:36:59 +0200

    login page view and dummy service call via http

--- src/app/login-reg/login-reg.module.ts
+++ src/app/login-reg/login-reg.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { ModuleWithProviders } from '@angular/core';
+import { LoginService } from "app/services/login-reg/login.service";
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [LoginComponent],
-  exports: [LoginComponent]
+  exports: [LoginComponent],
+  providers: [LoginService],
 })
 export class LoginRegModule { 
 
--- src/app/login-reg/login/login.component.ts
+++ src/app/login-reg/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from "app/services/login-reg/login.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  users;
 
-  constructor() { }
+  constructor(private loginService: LoginService) { 
+    this.users = loginService.getUsers();
+  }
 
   ngOnInit() {
   }
+
+  
 
 }
